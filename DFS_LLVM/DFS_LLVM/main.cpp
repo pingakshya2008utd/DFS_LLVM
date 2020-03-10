be7477f600aa6d31ab827c975a366af1476b5659
pingakshya2008utd <pingakshya2008@gmail.com>
2020-03-09 19:25:26 -0500

    main function changes

--- DFS_LLVM/DFS_LLVM/main.cpp
+++ DFS_LLVM/DFS_LLVM/main.cpp
@@ -19,21 +19,42 @@ struct DotEdge {
 };
 
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS, DotVertex, DotEdge> graph_t;
+//typedef boost::graph_traits<graph_t>::vertex_descriptor DotVertex;
 
+/*
 class MyVisitor : public boost::default_dfs_visitor {
 public:
-	MyVisitor() : vv(new std::vector<string>()) {}
+	MyVisitor() : vv(new std::vector<DotVertex>()) {}
 
-	void discover_vertex(int v, const graph_t &g) const {
-		vv->push_back(g[v].label);
+	void discover_vertex(DotVertex v, const graph_t &g) const {
+		vv->push_back(v);
 		return;
 	}
 
-	std::vector<string> &GetVector() const { return *vv; }
+	std::vector<DotVertex> &GetVector() const { return *vv; }
 
 private:
-	boost::shared_ptr<std::vector<string> > vv;
+	boost::shared_ptr<std::vector<DotVertex> > vv;
 };
+*/
+
+class MyVisitor : public boost::default_dfs_visitor {
+public:
+	MyVisitor() : vv(new std::vector<DotVertex>()) {}
+
+	void discover_vertex(DotVertex v, const graph_t& g) { //note the lack of const
+		//if (boost::in_degree(v, g) != 0) { //only print the vertices in the connected component (I already did MCC and removed edges so all the extra vertices are isolated)
+			std::cerr << v.label << std::endl;
+			vv->push_back(v);
+		//}
+		return;
+	}
+	std::vector<DotVertex>& GetVector() const { return *vv; }
+private:
+	boost::shared_ptr< std::vector<DotVertex> > vv;
+};
+
+
 
 int main() {
 	graph_t graphviz;
@@ -43,23 +64,25 @@ int main() {
 	dp.property("label", boost::get(&DotVertex::label, graphviz));
 	//dp.property("peripheries", boost::get(&DotVertex::peripheries, graphviz));
 	dp.property("label", boost::get(&DotEdge::label, graphviz));
-	std::ifstream dot("C:/Users/pxg131330/Downloads/acmart-master/input.dot");
+	std::ifstream dot("C:/Users/pxg131330/Downloads/acmart-master/cfg.main.dot");
 	//auto colormap = boost::make_vector_property_map<boost::default_color_type>(indexmap);
 
 	auto indexmap = boost::get(boost::vertex_index, graphviz);
 	auto colormap = boost::make_vector_property_map<boost::default_color_type>(indexmap);
+
 
 	bool status = boost::read_graphviz(dot, graphviz, dp);
 	if (status)
 		cout << 255;
 
 	MyVisitor vis;
-	boost::depth_first_search(graphviz, vis, colormap,1);
+	//boost::depth_first_search(graphviz, vis, colormap,1);
+	boost::depth_first_search(graphviz, boost::visitor(vis));
 
-	std::vector<string> vctr = vis.GetVector();
+	std::vector<DotVertex> vctr = vis.GetVector();
 
 	for (auto id : vctr)
-		std::cout << id << " ";
+		std::cout << id.label << " ";
 
 	system("pause");
 	return 0;
