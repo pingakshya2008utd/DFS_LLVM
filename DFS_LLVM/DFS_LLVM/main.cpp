ddb4a402deb14cdcad5fede0b4dadab33b7ebefa
pingakshya2008utd <pingakshya2008@gmail.com>
2020-03-11 20:06:30 -0500

    final for all files and foladers

--- DFS_LLVM/DFS_LLVM/main.cpp
+++ DFS_LLVM/DFS_LLVM/main.cpp
@@ -6,6 +6,7 @@
 #include <boost/graph/graph_utility.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/depth_first_search.hpp>
+#include <experimental/filesystem>
 
 
 using namespace boost;
@@ -59,63 +60,90 @@ private:
 };
 
 
-
 int main() {
-	graph_t graphviz;
-	boost::dynamic_properties dp(boost::ignore_other_properties);
-
-//	dp.property("node_id", boost::get(&DotVertex::name, graphviz));
-	dp.property("label", boost::get(&DotVertex::label, graphviz));
-	//dp.property("peripheries", boost::get(&DotVertex::peripheries, graphviz));
-	dp.property("label", boost::get(&DotEdge::label, graphviz));
-	std::ifstream dot("O:/total_new_apdcm/total_new_solution98/cfg.quantl.dot");
-	//std::ifstream dot("C:/Users/pxg131330/Downloads/acmart-master/cfg.main.dot");
-	//auto colormap = boost::make_vector_property_map<boost::default_color_type>(indexmap);
-
-	auto indexmap = boost::get(boost::vertex_index, graphviz);
-	auto colormap = boost::make_vector_property_map<boost::default_color_type>(indexmap);
-
-
-	bool status = boost::read_graphviz(dot, graphviz, dp);
-	if (status)
-		cout << 255;
-
 	
-
-	MyVisitor vis;
-	//boost::depth_first_search(graphviz, vis, colormap,1);
-	boost::depth_first_search(graphviz, boost::visitor(vis));
-
-	std::vector<DotVertex> vctr = vis.GetVector();
-
 	
+	vector <string> func_names{ "quantl", "encode", "decode", "reset", "upzero", "adpcm_main" };
+	file_counter = 1;
+	for (int i = 0; i < 100; i++) {
+		string directory_name = "total_new_solution " + to_string(i);
+		cout << directory_name << endl;
+		for (int j = 0; j < func_names.size(); j++) {
+			file_counter++;
+			string dot_file_name = "O:/total_new_apdcm/total_new_solution"+to_string(i)+"/cfg." + func_names[j] + ".dot";
 
-	/*
-	for (auto id : vctr)
-		std::cout << id.label << " ";
-	*/
+			bool exist = std::experimental::filesystem::exists(dot_file_name);
 
-	vector <int> indegree_vec, outdegree_vec;
+			if (!exist)
+				continue;
 
-	for (auto v : make_iterator_range(vertices(graphviz))) {
-	//	cout << graphviz[v].label << " has " << degree(v, graphviz) << " neighbor(s): ";
-		outdegree_vec.push_back((int)out_degree(v, graphviz));
-		indegree_vec.push_back((int)in_degree(v, graphviz));
+			func_name_for_res = func_names[j];
+			file_counter = i;
+			string result_file_name = "O:/total_new_apdcm/total_new_solution"+to_string(i)+"/" + func_names[j] + "_data.res";
+			graph_t graphviz;
+			boost::dynamic_properties dp(boost::ignore_other_properties);
 
-		
+			//	dp.property("node_id", boost::get(&DotVertex::name, graphviz));
+			dp.property("label", boost::get(&DotVertex::label, graphviz));
+			//dp.property("peripheries", boost::get(&DotVertex::peripheries, graphviz));
+			dp.property("label", boost::get(&DotEdge::label, graphviz));
+			//std::ifstream dot("O:/total_new_apdcm/total_new_solution98/cfg.quantl.dot");
+			std::ifstream dot(dot_file_name);
+			//auto colormap = boost::make_vector_property_map<boost::default_color_type>(indexmap);
+
+			auto indexmap = boost::get(boost::vertex_index, graphviz);
+			auto colormap = boost::make_vector_property_map<boost::default_color_type>(indexmap);
+
+
+			bool status = boost::read_graphviz(dot, graphviz, dp);
+			if (status)
+				cout << status<<endl;
+
+
+
+			MyVisitor vis;
+			//boost::depth_first_search(graphviz, vis, colormap,1);
+			boost::depth_first_search(graphviz, boost::visitor(vis));
+
+			std::vector<DotVertex> vctr = vis.GetVector();
+
+
+
+			/*
+			for (auto id : vctr)
+				std::cout << id.label << " ";
+			*/
+
+			vector <int> indegree_vec, outdegree_vec;
+			std::ofstream outfile(result_file_name, std::ios_base::app);
+
+			for (auto v : make_iterator_range(vertices(graphviz))) {
+				//	cout << graphviz[v].label << " has " << degree(v, graphviz) << " neighbor(s): ";
+				outdegree_vec.push_back((int)out_degree(v, graphviz));
+				indegree_vec.push_back((int)in_degree(v, graphviz));
+
+
+			}
+
+			int max_outedges = *max_element(outdegree_vec.begin(), outdegree_vec.end());
+			int max_inedges = *max_element(indegree_vec.begin(), indegree_vec.end());
+			int total_nodes = vctr.size();
+
+			//	cout << " max number of outgoing edges = " << max_outedges << endl;
+			//	cout << " max number of incoming edges = " << max_inedges << endl;
+			//	cout << " total number of nodes= " << total_nodes << endl;
+
+
+			outfile << " max number of outgoing edges = " << max_outedges << endl;
+			outfile << " max number of incoming edges = " << max_inedges << endl;
+			outfile << " total number of nodes= " << total_nodes << endl;
+
+			outdegree_vec.clear();
+			indegree_vec.clear();
+			vctr.clear();
+			outfile.close();
+		}
 	}
-
-	int max_outedges = *max_element(outdegree_vec.begin(), outdegree_vec.end());
-	int max_inedges = *max_element(indegree_vec.begin(), indegree_vec.end());
-	int total_nodes = vctr.size();
-
-	cout << " max number of outgoing edges = " << max_outedges << endl;
-	cout << " max number of incoming edges = " << max_inedges << endl;
-	cout << " total number of nodes= " << total_nodes << endl;
-
-	outdegree_vec.clear();
-	indegree_vec.clear();
-	vctr.clear();
 
 	system("pause");
 	return 0;
